baba4e84cacac5f3ae462836475e3b962aa6ce47
sahilram24 <chsahil.ram24@gmail.com>
2025-01-07 20:18:27 +0530

    converting into repo

---
+++ conditions.js
@@ -0,0 +1,79 @@
+// if(true)
+// {
+//     console.log("if block is executing");
+// }
+
+// if(false){
+//     console.log("true block");
+// }else{
+//     console.log("false block");
+// }
+
+//let isAdult = 10;
+//if(isAdult >= 18){
+//    console.log("He/She is major");
+//}else{
+//    console.log("He/She is minor");
+//}
+//
+//isAdult >= 18 ?console.log("Major"):console.log("Minor");
+
+//let day = prompt("Enter a day value");
+//if(day == 1){
+//    console.log("Sun");
+//}else if(day == 2){
+//    console.log("Mon");
+//}else if(day == 3){
+//    console.log("Tue");
+//}else if(day == 4){
+//    console.log("Wed");
+//}else if(day == 5){
+//    console.log("Thu");
+//}else if(day == 6){
+//    console.log("Fri");
+//}else if(day == 7){
+//    console.log("Sat");
+//}
+//else{
+//    console.log("Invalid day");
+//}
+
+// let day = Number(prompt("Enter a day value"));
+// console.log(day , typeof(day));
+// switch (day) {
+//     case 1:
+//         console.log("Sun");
+//         break;
+//         case 2:
+//         console.log("Mon");
+//         break;
+//         case 3:
+//         console.log("Tue");
+//         break;
+//         case 4:
+//         console.log("Wed");
+//         break;
+//         case 5:
+//         console.log("Thu");
+//         break;
+//         case 6:
+//         console.log("Fri");
+//         break;
+//         case 7:
+//         console.log("Sat");
+//         break;
+
+//     default:console.log("Invalid day");
+//         break;
+// }
+
+// write a condition to check gretaest numbers among 3 numbers
+// let a =20;
+// let b=30;
+//let c=40;
+
+// console.log("Hello World!!!");
+// console.log("Hello World!!!");
+// console.log("Hello World!!!");
+// console.log("Hello World!!!");
+// console.log("Hello World!!!");
---
+++ operators.js
@@ -0,0 +1,69 @@
+console.log(5+5);
+console.log(100+1000);
+console.log(10+"100"); //concatenation
+console.log(10+20+"hii");
+console.log("hii"+10+50);
+console.log("hi"+(10+50));
+
+console.log(10-5);
+console.log("10"-5); //!typecasting
+console.log("10"-"2");
+
+console.log(5*5);
+console.log(5*"5"); //!typecasting
+
+console.log(10/5);
+console.log("10"/5);//!typecasting
+
+console.log(2**5);
+console.log(5**2);
+console.log("5"**"5");//!typecasting
+
+console.log(10%2);
+console.log("10"%5);//!typecasting
+
+//!Increment
+//let a=10;
+//console.log(a);//10
+//console.log(++a);//11
+//console.log(a++);//11
+//console.log(a);//12
+
+
+//decrement
+//console.log(--a);//9
+//console.log(a--);//10
+//console.log(a);//9
+
+
+//console.log(a+5);
+//console.log(a+=5);// a = a+5
+//console.log(a-=2);
+//console.log(a*=2);
+//console.log(a**=5);
+//console.log(a%=2);
+
+//console.log(10 == 10);
+//console.log("10"==10);
+//console.log(100==10);
+//console.log("hi"=="hi");
+
+
+ //console.log(10===10);
+ //console.log("10"===10);
+
+ //console.log(10!=5);
+ //console.log(100!="100");
+ //console.log(10!==10);
+ //console.log("10" !== 10);
+
+ //console.log(10>6);
+ //console.log(10>=10);
+ //console.log(10<20);
+ //console.log(20<=20);
+
+//console.log((10>5)&&(5>10));
+//console.log((10>5)&&(5<10));
+
+//console.log(!true);
+//console.log(!false);
