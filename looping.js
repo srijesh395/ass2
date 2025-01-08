12ef82c14985b1cea2644d4209fc9d1675d89a92
sahilram24 <chsahil.ram24@gmail.com>
2025-01-08 13:40:24 +0530

    adding files

---
+++ looping.js
@@ -0,0 +1,16 @@
+for (let i=0;i<=100;i++){
+    if(i%2 ==0){
+        console.log(i +"even");  
+    }
+    else{
+        console.log(i +"odd");
+        
+    }
+    
+}
+
+let mul=1;
+let num = number(prompt("Enter a number"))
+for(let i=1;i<=num;i++){
+    mul = mul*i;
+}
